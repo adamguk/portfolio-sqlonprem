ad2b321d989ccc7c2d40ea8899a54c7e4b4638af
adamguk <adam.e.garnham@gmail.com>
2026-08-11 19:02:41 +0100

    FEAT: Change deployment to dedicated container

--- deploy.ps1
+++ deploy.ps1
@@ -8,22 +8,20 @@ $Credential = Get-Credential -UserName "sa" -Message "Enter dbserver Password"
 $Username = $Credential.UserName
 $Password = $Credential.GetNetworkCredential().Password
 
-$Server = "localhost"
+$Server = "192.168.50.185"
 $Port = "1433"
-$Database = "DWH_ONPREM_DEV"
-
-# Combine server and port (e.g., "localhost,1433")
+$Database = "DWH_ONPREM"
 $ServerInstance = "$Server,$Port"
 
-# Define deployment sequence in order
 $DeploymentSteps = @(
     @{ Name = "Database & Schemas"; Path = ".\scripts\01_init_db.sql"; UseTargetDB = $false },
     @{ Name = "Staging Tables"; Path = ".\scripts\02_create_tables_staging.sql"; UseTargetDB = $true },
     @{ Name = "Intermediate Views"; Path = ".\scripts\03_create_views_intermediate.sql"; UseTargetDB = $true },
-    @{ Name = "Mart Tables"; Path = ".\scripts\04_create_tables_mart.sql"; UseTargetDB = $true }
+    @{ Name = "Mart Tables"; Path = ".\scripts\04_create_tables_mart.sql"; UseTargetDB = $true },
+    @{ Name = "Procedures"; Path = ".\scripts\05_create_procedures.sql"; UseTargetDB = $true },
+    @{ Name = "AgentJobs"; Path = ".\scripts\06_create_agent_jobs.sql"; UseTargetDB = $false }
 )
 
-# Run through each script in order
 foreach ($Step in $DeploymentSteps) {
     Write-Host ""
     Write-Host "Deploying: $($Step.Name)..." 
@@ -42,8 +40,6 @@ foreach ($Step in $DeploymentSteps) {
         Write-Host ""
         Write-Host "[FATAL ERROR] Deployment aborted during step: $($Step.Name)"
         Write-Host $_.Exception.Message -ForegroundColor Red
-        
-        # Stop whole script immediately on failure
         break
     }
 }
